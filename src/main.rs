d047824efb9b112814590d2219e02dfbe61eb86d
Emre Besirik <nomercy@gmail.com>
2025-11-08 19:05:07 +0300

    Accepting implicit parameters

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,25 @@ fn main() {
     } else if let Some(long) = args.long_to_guid {
         let guid = Uuid::from_u128(long);
         println!("Long: {}\nGuid: {}", long, guid);
+    } else if let Some(value) = args.value {
+        // Try parsing as UUID first
+        if let Ok(guid) = value.parse::<Uuid>() {
+            let long = guid.as_u128();
+            println!("Guid: {}\nLong: {}", guid, long);
+        }
+        // Try parsing as u128
+        else if let Ok(long) = value.parse::<u128>() {
+            let guid = Uuid::from_u128(long);
+            println!("Long: {}\nGuid: {}", long, guid);
+        }
+        // Neither worked
+        else {
+            eprintln!(
+                "Error: Unable to parse '{}' as either a UUID or a number",
+                value
+            );
+            std::process::exit(1);
+        }
     } else {
         panic!("Missing arguments!");
     }
@@ -23,4 +42,7 @@ pub struct Args {
 
     #[arg(short, long)]
     pub long_to_guid: Option<u128>,
+
+    /// The value to convert (either a UUID or a number)
+    pub value: Option<String>,
 }
